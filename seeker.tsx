0ff5e25ede000c22ac464d6df2f7aeb0186d4425
Andrew Moore <andrew.moore@samdesk.io>
2026-04-06 08:55:31 -0600

    feat: add cmd functions

--- seeker.tsx
+++ seeker.tsx
@@ -1,11 +1,26 @@
+#!/usr/bin/env tsx
+
+import { execFileSync, spawnSync } from "child_process";
 import fs from "fs";
+import path from "path";
+
+interface Cache {
+  query: string;
+  results: string[];
+}
+
+interface SearchOptions {
+  caseInsensitive?: boolean;
+  filenameOnly?: boolean;
+  showAll?: boolean;
+}
 
 const maxResults = 50;
+
 if (!process.getuid) {
-  throw new Error(
-    "Windows is not supported due to UID-based directory isolation",
-  );
+  throw new Error("File system is not supported.");
 }
+
 const cacheDirectory = `/tmp/seeker/${process.getuid()}`;
 const cacheFile = `${cacheDirectory}/results.json`;
 
@@ -24,7 +39,62 @@ function ensureCacheDirectory() {
   }
 }
 
-function saveCache(query: string, results: unknown[]) {
+function saveCache(query: string, results: string[]) {
   ensureCacheDirectory();
   fs.writeFileSync(cacheFile, JSON.stringify({ query, results }, null, 2));
+}
+
+function loadCache(): Cache {
+  if (!fs.existsSync(cacheFile)) {
+    console.error("No previous search - run: skr <query> to get started");
+    process.exit(1);
+  }
+  return JSON.parse(fs.readFileSync(cacheFile, "utf-8")) as Cache;
+}
+
+function pickLine(num: number): string {
+  const cache = loadCache();
+  const results = cache.results;
+  if (num < 1 || num > results.length) {
+    console.error(`Number ${num} out of range (1-${results.length})`);
+    process.exit(1);
+  }
+  return results[num - 1];
+}
+
+function hasCommand(name: string): boolean {
+  try {
+    execFileSync("which", [name], { stdio: "ignore" });
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function buildFindCommand(query: string, caseInsensitive: boolean): string[] {
+  if (hasCommand("fd")) {
+    const cmd = ["fd", query];
+    if (caseInsensitive) cmd.push("--ignore-case");
+    return cmd;
+  }
+  const nameFlag = caseInsensitive ? "-iname" : "-name";
+  return ["find", ".", "-not", "-path", "./.git/*", nameFlag, `*${query}*`];
+}
+
+function buildGrepCommand(query: string, caseInsensitive: boolean): string[] {
+  if (hasCommand("rg")) {
+    const cmd = ["rg", "--files-with-matches"];
+    if (caseInsensitive) cmd.push("--ignore-case");
+    cmd.push(query);
+    return cmd;
+  }
+  const cmd = [
+    "grep",
+    "-rl",
+    "--exclude-dir=.git",
+    "--binary-files=without-match",
+  ];
+  if (caseInsensitive) cmd.push("-i");
+  cmd.push(query);
+  return cmd;
 }
